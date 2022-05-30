c6357ca1948fc2e73fa983ce490b05192907d8e5
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2022-05-30 15:50:49 -0300

    Create grupo.sh

---
+++ grupo.sh
@@ -0,0 +1,11 @@
+{
+    \"jsonrpc\": \"2.0\",
+    \"method\": \"host.update\",
+    \"params\": {
+        \"hostid\": \"${GET_HOST_ID}\",
+        \"groups\": [
+        {
+                \"groupid\":\"${GROUP_ID}\"
+        }
+        ]
+    }
